b952b33ae5d17ac25cd1c26ddf728fc55485b097
Matan <matansidon@gmail.com>
2020-10-30 16:01:58 +0200

    first change

--- Controller.py
+++ Controller.py
@@ -1,8 +1,12 @@
 from Model import Model
 from View import View
-
+from tkinter import messagebox
 
 class Controller:
+
+    # X starts so true
+    clicked = True
+    count = 0
 
     def __init__(self):
         self.model = Model()
@@ -13,3 +17,18 @@ class Controller:
 
     def on_button_click(self,text):
         print(f"button {text} clicked")
+
+    def b_click(self,b):
+        global clicked, count
+            if b["text"] == " " and clicked == True:
+                b["text"] = "X"
+                clicked = False
+                count += 1
+               # checkifwon()
+            elif b["text"] == " " and clicked == False:
+                b["text"] = "O"
+                clicked = True
+                count += 1
+               # checkifwon()
+            else:
+                #messagebox.showerror("Tic Tac Toe", "Hey! That box has already been selected\nPick Another Box...")
