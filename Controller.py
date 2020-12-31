a1905a64d442414afc5d80c66f7b05ff0112dc26
tzufstokelman <tzufstokelman@gmail.com>
2020-12-31 10:48:12 +0200

    שערשעכגע

--- Controller.py
+++ Controller.py
@@ -1,9 +1,9 @@
-from FileHandler import FileHandler
 from GameCaretaker import *
 from GameMemento import *
 from Model import *
 from Player import *
 from View import *
+import operator
 
 class Controller:
 
@@ -16,15 +16,14 @@ class Controller:
         self.view.main()
 
     def new_game_clicked(self):
-        self.view.create_new_game_page()
+        # dont start new game if a previous game is still running
+        if self.model.game_state == GameState.ACTIVE.value or self.model.game_state == GameState.CHOOSE_PLAYERS_DETAILS.value:
+            return
 
-        # create GameCaretaker and first memento
-        self.care_taker = GameCaretaker()
-        self.care_taker.add_memento(GameMemento(self.model.game_data_grid))
-
-        # for testing
-        self.model.player1 = Player("tal",Shape.TRIANGLE,Color.RED,Size.MEDIUM)
-        self.model.player2 = Player("talya", Shape.CIRCLE,Color.BLUE,Size.LARGE)
+        self.model.game_state = GameState.CHOOSE_PLAYERS_DETAILS.value
+        userswins_from_file = FileHandler.get_all_users_and_wins()
+        users_from_file = list(userswins_from_file.keys())
+        self.view.create_new_game_page(users_from_file)
 
     def add_new_user_clicked(self):
         self.view.create_new_user_page()
@@ -32,7 +31,35 @@ class Controller:
     def generate_report_clicked(self):
         pass
 
-    def start_game_clicked(self):
+    def start_game_clicked(self,root):
+        # create player 1
+        player1_name = self.view.player1_name_combo.get()
+        player1_shape = self.view.player1_shape_combo.get()
+        player1_color = self.view.player1_color_combo.get()
+        player1_size = self.view.player1_size_combo.get()
+        player1 = Player(player1_name,player1_shape,player1_color,player1_size)
+
+        # create player 2
+        player2_name = self.view.player2_name_combo.get()
+        player2_shape = self.view.player2_shape_combo.get()
+        player2_color = self.view.player2_color_combo.get()
+        player2_size = self.view.player2_size_combo.get()
+        player2 = Player(player2_name,player2_shape,player2_color,player2_size)
+
+        # verify players
+        players_valid_val = self.model.players_valid(player1,player2)
+        if players_valid_val != "":
+            self.view.show_error(players_valid_val)
+            return
+
+        root.destroy()
+        self.model.player1 = player1
+        self.model.player2 = player2
+
+        # create GameCaretaker and first memento
+        self.care_taker = GameCaretaker()
+        self.care_taker.add_memento(GameMemento(self.model.game_data_grid))
+
         self.model.start_new_game()
         self.view.create_game_board_page(self.model.game_data_grid,self.model.game_board_message)
 
@@ -42,10 +69,9 @@ class Controller:
 
     def on_resize(self,event):
         self.view.redraw_game_board(self.model.game_data_grid,self.model.game_board_message)
-        print(f"resize occurred")
 
     def cell_clicked(self,event):
-        if self.view.game_board_canvas.find_withtag(CURRENT) and self.model.game_state.value == GameState.ACTIVE.value:
+        if self.view.game_board_canvas.find_withtag(CURRENT) and self.model.game_state == GameState.ACTIVE.value:
             tag = self.view.game_board_canvas.itemcget(CURRENT, "tags")
             row_column = re.split(',| ', tag)
             # row_column = tag.split(sep=',')
@@ -55,37 +81,35 @@ class Controller:
             self.model.move(row,column)
             self.care_taker.add_memento(GameMemento(self.model.game_data_grid))
             self.view.redraw_game_board(self.model.game_data_grid,self.model.game_board_message)
-            print(f"cell {row},{column} was clicked")
 
     # activated when redo button clicked
     def redo_clicked(self):
-        print("redo_clicked")
-        if(self.model.game_state.value == GameState.ACTIVE.value):
+        if self.model.game_state == GameState.ACTIVE.value:
             memento = self.care_taker.redo()
-            if memento != None:
+            if memento is not None:
                 self.model.update_grid_by_memento(memento)
             self.view.redraw_game_board(self.model.game_data_grid,self.model.game_board_message)
 
     # activated when undo button clicked
     def undo_clicked(self):
-        print("undo_clicked")
-        if (self.model.game_state.value == GameState.ACTIVE.value):
+        if self.model.game_state == GameState.ACTIVE.value:
             memento = self.care_taker.undo()
-            if memento != None:
+            if memento is not None:
                 self.model.update_grid_by_memento(memento)
             self.view.redraw_game_board(self.model.game_data_grid,self.model.game_board_message)
 
+    # insert new user to the file (if the user name is valid)
     def insert_new_user_clicked(self):
         name_entered = self.view.new_user_entry_str.get()
-        insert_user_result = self.model.check_new_user_name(name_entered)
-        message = None
-        if insert_user_result == InsertNewUserResulr.INSERT_NEW_USER_RESULT_INVALID:
-            message = f"Invalid name !!"
-        elif insert_user_result == InsertNewUserResulr.INSERT_NEW_USER_RESULT_USER_EXISTS:
-            message = f"User {name_entered} exists, choose a different name"
+        if FileHandler.user_name_exists_in_file(name_entered):
+            self.view.new_user_add_error_label_str.set(Error.USER_EXISTS.value)
         else:
-            message = f"User {name_entered} was added successfully"
             FileHandler.add_new_user_to_file(name_entered)
+            self.view.new_user_add_error_label_str.set(f"User {name_entered} successfully added to file")
 
-        self.view.new_user_add_error_label_str.set(message)
-
+    def generate_report_clicked(self):
+        users_wins = FileHandler.get_all_users_and_wins()
+        sort_users_wins = sorted(users_wins.items(), key=operator.itemgetter(1), reverse=True)
+        str_to_print = ""
+        for item in sort_users_wins: str_to_print = str_to_print + item[0] + ' : ' + str(item[1]) + "\n"
+        self.view.show_report(str_to_print)
--- View.py
+++ View.py
@@ -1,9 +1,8 @@
 from tkinter import *
-from tkinter import font
+from tkinter import font, messagebox
 from tkinter.font import Font
 from tkinter.ttk import Combobox
 
-from Constants import *
 from Constants import Size, Shape, Color
 
 
@@ -27,6 +26,7 @@ class View:
         self.player2_shape_combo = None
         self.player2_color_combo = None
         self.player2_size_combo = None
+        self.player1_labelframe = None
 
     def main(self):
         self.create_start_page()
@@ -60,8 +60,8 @@ class View:
 
         root.mainloop()
 
-    def create_new_game_page(self):
-        height = 520
+    def create_new_game_page(self,users):
+        height = 420
         width = 380
         root = Toplevel()
         root.geometry(f"{width}x{height}")
@@ -76,66 +76,103 @@ class View:
         colors = [Color.BLUE.value, Color.RED.value, Color.GREEN.value]
         sizes = [Size.SMALL.value, Size.MEDIUM.value, Size.LARGE.value]
         lableframe_title_font = Font(family="Helvetica", size=20, weight="bold")
-        lable_font = Font(family="Helvetica", size=15)
-        combo_font = Font(family="Helvetica", size=10)
-        padx = 5
-        pady = 5
+        lable_font = combo_font = Font(family="Helvetica", size=15)
+        padx = pady = 5
 
         ### first labelframe
-        player1_labelframe = LabelFrame(self.new_game_page, text="player 1", font = lableframe_title_font)
+        player1_labelframe = LabelFrame(self.new_game_page, text="player 1", font=lableframe_title_font)
         player1_labelframe.pack(fill="both", expand="yes", pady=15)
         # name
-        player1_name_label = Label(player1_labelframe, text="Name:",font=lable_font).grid(row=0, column=0,padx=padx, pady=pady)
-        self.player1_name_combo = Combobox(player1_labelframe,font=combo_font).grid(row=0, column=1,padx=padx, pady=pady)
+        player1_name_label = Label(player1_labelframe, text="Name:", font=lable_font).grid(row = 0, column = 0)
+        self.player1_name_combo = Combobox(player1_labelframe, font=combo_font, values=users)
+        self.player1_name_combo.grid(row = 0, column = 1)
         # shape
-        player1_shape_label = Label(player1_labelframe, text="Shape:",font=lable_font).grid(row=1, column=0,padx=padx, pady=pady)
-        self.player1_shape_combo = Combobox(player1_labelframe, values=shapes,font=combo_font).grid(row=1, column=1,padx=padx, pady=pady)
+        player1_shape_label = Label(player1_labelframe, text="Shape:", font=lable_font).grid(row = 1, column = 0)
+        self.player1_shape_combo = Combobox(player1_labelframe, values=shapes, font=combo_font)
+        self.player1_shape_combo.grid(row = 1, column = 1)
         # color
-        player1_color_label = Label(player1_labelframe, text="Color:",font=lable_font).grid(row=2, column=0,padx=padx, pady=pady)
-        self.player1_color_combo = Combobox(player1_labelframe, values=colors,font=combo_font).grid(row=2, column=1,padx=padx, pady=pady)
+        player1_color_label = Label(player1_labelframe, text="Color:", font=lable_font).grid(row = 2, column = 0)
+        self.player1_color_combo = Combobox(player1_labelframe, values=colors, font=combo_font)
+        self.player1_color_combo.grid(row = 2, column = 1)
         # size
-        player1_shape_label = Label(player1_labelframe, text="Size:",font=lable_font).grid(row=3, column=0,padx=padx, pady=pady)
-        self.player1_shape_combo = Combobox(player1_labelframe, values=sizes,font=combo_font).grid(row=3, column=1,padx=padx, pady=pady)
+        player1_shape_label = Label(player1_labelframe, text="Size:", font=lable_font).grid(row = 3, column = 0)
+        self.player1_size_combo = Combobox(player1_labelframe, values=sizes, font=combo_font)
+        self.player1_size_combo.grid(row = 3, column = 1)
 
         ### second labelframe
-        player2_labelframe = LabelFrame(self.new_game_page, text="player 2",font = lableframe_title_font)
-        player2_labelframe.pack(fill="both", expand="yes",pady=15)
+        player2_labelframe = LabelFrame(self.new_game_page, text="player 2", font=lableframe_title_font)
+        player2_labelframe.pack(fill="both", expand="yes", pady=15)
         # name
-        player2_name_label = Label(player2_labelframe, text="Name:",font=lable_font).grid(row=0, column=0,padx=padx, pady=pady)
-        self.player2_name_combo = Combobox(player2_labelframe,font=combo_font).grid(row=0, column=1,padx=padx, pady=pady)
+        player2_name_label = Label(player2_labelframe, text="Name:", font=lable_font).grid(row = 0, column = 0)
+        self.player2_name_combo = Combobox(player2_labelframe, font=combo_font, values=users)
+        self.player2_name_combo.grid(row = 0, column = 1)
         # shape
-        player2_shape_label = Label(player2_labelframe, text="Shape:",font=lable_font).grid(row=1, column=0,padx=padx, pady=pady)
-        self.player2_shape_combo = Combobox(player2_labelframe, values=shapes,font=combo_font).grid(row=1, column=1,padx=padx, pady=pady)
+        player2_shape_label = Label(player2_labelframe, text="Shape:", font=lable_font).grid(row = 1, column = 0)
+        self.player2_shape_combo = Combobox(player2_labelframe, values=shapes, font=combo_font)
+        self.player2_shape_combo.grid(row = 1, column = 1)
         # color
-        player2_color_label = Label(player2_labelframe, text="Color:",font=lable_font).grid(row=2, column=0,padx=padx, pady=pady)
-        self.player2_color_combo = Combobox(player2_labelframe, values=colors,font=combo_font).grid(row=2, column=1,padx=padx, pady=pady)
+        player2_color_label = Label(player2_labelframe, text="Color:", font=lable_font).grid(row = 2, column = 0)
+        self.player2_color_combo = Combobox(player2_labelframe, values=colors, font=combo_font)
+        self.player2_color_combo.grid(row = 2, column = 1)
         # size
-        player2_shape_label = Label(player2_labelframe, text="Size:",font=lable_font).grid(row=3, column=0,padx=padx, pady=pady)
-        self.player2_shape_combo = Combobox(player2_labelframe, values=sizes,font=combo_font).grid(row=3, column=1,padx=padx, pady=pady)
+        player2_shape_label = Label(player2_labelframe, text="Size:", font=lable_font).grid(row = 3, column = 0)
+        self.player2_size_combo = Combobox(player2_labelframe, values=sizes, font=combo_font)
+        self.player2_size_combo.grid(row = 3, column = 1)
 
         # start game button
-        start_game_but = Button(self.new_game_page, text="Start Game",font = lable_font, command=self.controller.start_game_clicked)
-        start_game_but.pack(side=LEFT)
+        start_game_but = Button(self.new_game_page, text="Start Game",font = lable_font, command=lambda:self.controller.start_game_clicked(root))
+        start_game_but.pack(side = "left")
 
+    def create_new_user_page(self):
+        height = 600
+        width = 400
+        root = Toplevel()
+        root.geometry(f"{width}x{height}")
+        root.title("Add new user")
+        root.wm_attributes("-topmost", 1)
+        self.new_user_page = Frame(root, height=height, width=width)
+        self.new_user_page.pack()
+
+        font = Font(family='Helvetica', size=15, weight='bold')
+        new_user_label = Label(self.new_user_page,text = 'Enter new user name:',font = font)
+        new_user_label.pack(side = LEFT)
+        self.new_user_entry_str = StringVar()
+        new_user_entry = Entry(self.new_user_page, textvariable=self.new_user_entry_str)
+        new_user_entry.pack(side = LEFT)
+        add_button = Button(self.new_user_page, text="Add",command=self.controller.insert_new_user_clicked,font = font)
+        add_button.pack(side = LEFT)
+        self.new_user_add_error_label_str = StringVar()
+        new_user_add_error_label = Label(self.new_user_page,textvariable = self.new_user_add_error_label_str,font = font,fg = "red")
+        new_user_add_error_label.pack(side = BOTTOM,anchor=W)
+
+    def create_report(self):
+        pass
+    
     def create_game_board_page(self, game_data_grid, text_message):
         root = Toplevel()
         root.title("Game Board")
+        root.wm_attributes("-topmost", 1)
+        height = 510
+        width = 500
+        root.geometry(f"{width}x{height}")
 
         # top message label
+        font = Font(family='Helvetica', size=15, weight='bold')
         self.game_board_message = StringVar()
-        message_label = Label(root, textvariable= self.game_board_message)
-        message_label.pack(expand=TRUE,fill=BOTH)
+        message_label = Label(root,font = font,textvariable= self.game_board_message)
+        message_label.pack()
 
         # grid canvas
         #self.game_board_canvas = Canvas(root, height=canvas_height, width=canvas_width)
         self.game_board_canvas = Canvas(root)
         self.game_board_canvas.bind("<Configure>", self.controller.on_resize)
+        self.game_board_canvas.pack_propagate(0)
         self.game_board_canvas.pack(expand=TRUE, fill=BOTH)
 
         # redo undo buttons
-        undo_button = Button(root, text="undo",command = self.controller.undo_clicked)
+        undo_button = Button(root, text="undo",command = self.controller.undo_clicked,font = font)
         undo_button.pack(expand=TRUE, fill=BOTH,side = LEFT)
-        redo_button = Button(root, text="redo",command = self.controller.redo_clicked)
+        redo_button = Button(root, text="redo",command = self.controller.redo_clicked,font = font)
         redo_button.pack(expand=TRUE, fill=BOTH, side=LEFT)
 
         self.redraw_game_board(game_data_grid,text_message)
@@ -169,14 +206,15 @@ class View:
                 if(player != None):
                     self.draw_shape(player,x1,y1,cellwidth,cellheight)
 
+    # draw the shape (RECTANGLE/CIRCLE/TRIANGLE) in the correct cell by the player details on the game board
     def draw_shape(self, the_player, cell_topleft_x, cell_topleft_y, cell_width, cell_height):
 
         shape = the_player.shape
-        color = the_player.color.value
+        color = the_player.color
         div_param = 7
-        if the_player.size == Size.LARGE:
+        if the_player.size == Size.LARGE.value:
             size_param = 1
-        elif the_player.size == Size.MEDIUM:
+        elif the_player.size == Size.MEDIUM.value:
             size_param = 2
         else:
             size_param = 3
@@ -185,50 +223,27 @@ class View:
         y1 = cell_topleft_y + ((cell_height / div_param) * size_param)
         x2 = cell_topleft_x + cell_width - ((cell_width / div_param) * size_param)
         y2 = cell_topleft_y + cell_height - ((cell_height / div_param) * size_param)
-        if shape == Shape.RECTANGLE:
+        if shape == Shape.RECTANGLE.value:
             self.game_board_canvas.create_rectangle(x1, y1, x2, y2, fill=color)
-        elif shape == Shape.CIRCLE:
+        elif shape == Shape.CIRCLE.value:
             self.game_board_canvas.create_oval(x1, y1, x2, y2, fill=color)
         else:
             points = [x1, y2, x2, y2, ((x1 + x2) / 2), y1]
             self.game_board_canvas.create_polygon(points, fill=color)
 
-    def create_new_user_page(self):
-        height = 600
-        width = 400
+
+    def show_error(self,error_msg):
+        messagebox.showerror("Error", error_msg)
+
+    def show_report(self, str_to_print):
+        lable_font = Font(family="Helvetica", size=15)
         root = Toplevel()
-        root.title("New new user")
-        self.new_user_page = Frame(root, height=height, width=width)
-        self.new_user_page.pack()
-
-        new_user_label = Label(self.new_user_page,text = 'Enter new user name:')
-        new_user_label.grid(column=0, row=0)
-        self.new_user_entry_str = StringVar()
-        new_user_entry = Entry(self.new_user_page, textvariable=self.new_user_entry_str)
-        new_user_entry.grid(column=1, row=0)
-        add_button = Button(self.new_user_page, text="Add",command=self.controller.insert_new_user_clicked)
-        add_button.grid(column=2, row=0)
-        self.new_user_add_error_label_str = StringVar()
-        new_user_add_error_label = Label(self.new_user_page,textvariable = self.new_user_add_error_label_str)
-        new_user_add_error_label.grid(row=1)
-
-    def create_labelframe(self,player):
-        # shapes, colors and sizes lists
-        shapes = [Shape.CIRCLE.value, Shape.RECTANGLE.value, Shape.TRIANGLE.value]
-        colors = [Color.BLUE.value, Color.RED.value, Color.GREEN.value]
-        sizes = [Size.SMALL.value, Size.MEDIUM.value, Size.LARGE.value]
-
-        player1_labelframe = LabelFrame(self.new_game_page, text=player)
-        player1_labelframe.pack(fill="both", expand="yes")
-        # name
-        player1_name_label = Label(player1_labelframe, text="Name:").grid(row=0, column=0)
-        self.player1_name_combo = Combobox(player1_labelframe).grid(row=0, column=1)
-        # shape
-        self.player1_shape_label = Label(player1_labelframe, text="Shape:").grid(row=1, column=0)
-        self.player1_shape_combo = Combobox(player1_labelframe, values=shapes).grid(row=1, column=1)
-        # color
-        self.player1_color_label = Label(player1_labelframe, text="Color:").grid(row=2, column=0)
-        self.player1_color_combo = Combobox(player1_labelframe, values=colors).grid(row=2, column=1)
-        # size
-        self.player1_shape_label = Label(player1_labelframe, text="Size:").grid(row=3, column=0)
-        self.player1_shape_combo = Combobox(player1_labelframe, values=sizes).grid(row=3, column=1)
+        # root.geometry(f"{width}x{height}")
+        # root.resizable(width=False, height=False)
+        root.title("Report")
+        root.wm_attributes("-topmost", 1)
+        report = Frame(root)
+        report.pack()
+        report_label = Label(report, text=str_to_print, font=lable_font)
+        report_label.pack()
+        report_label.pack_propagate(0)
